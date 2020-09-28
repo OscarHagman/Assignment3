9ff894cb4fbb8bcc13746d30f1d35957d64e0d6d
Oscar Hagman <Oscar.Leslie.Hagman@gmail.com>
2020-09-28 12:10:23 +0200

    Initial commit

---
+++ src/com/company/MyClass.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MyClass {
+
+    public static void myMethod() {
+        try {
+            System.out.println("Hello there");
+        }
+        catch (Exception e) {
+            System.out.println("General kenobi...");
+        }
+    }
+}
